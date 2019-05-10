e308642d0c23f6a2745f8648003ff092c7315b3b
HerbertLiskin <gkljatsko@gmail.com>
2019-05-10 14:12:40 +0300

    Updates

---
+++ precache-manifest.a05013ea9fdc5837644929e8e62a7f2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87881cfbde638ca348f42a254a21689a",
+    "url": "/hooks-and-typescript-test/index.html"
+  },
+  {
+    "revision": "46d855d1384bf879b492",
+    "url": "/hooks-and-typescript-test/static/css/main.a8fb4010.chunk.css"
+  },
+  {
+    "revision": "c4dcfb7c39dce39fd27e",
+    "url": "/hooks-and-typescript-test/static/js/2.03aa08cb.chunk.js"
+  },
+  {
+    "revision": "46d855d1384bf879b492",
+    "url": "/hooks-and-typescript-test/static/js/main.aa1a54c4.chunk.js"
+  },
+  {
+    "revision": "b4f34574f556d6d357a0",
+    "url": "/hooks-and-typescript-test/static/js/runtime~main.40fcd203.js"
+  },
+  {
+    "revision": "c372e0c9fd7ca4c14933c6216fb36b50",
+    "url": "/hooks-and-typescript-test/static/media/star.c372e0c9.svg"
+  }
+]);
